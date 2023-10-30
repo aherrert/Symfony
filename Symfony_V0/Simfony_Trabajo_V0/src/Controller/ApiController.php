d55a99273ea414356b811d7f31f4530e6f792957
aherrert <77837668+aherrert@users.noreply.github.com>
2023-10-30 16:07:46 +0100

    first commit

---
+++ Symfony_V0/Simfony_Trabajo_V0/src/Controller/ApiController.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController
+{
+
+    /**
+     * @Route("/about-us", name="api_about_us")
+     */
+    public function getAboutUsInfo(): JsonResponse
+    {
+        $aboutUsInfo = [
+            'title' => 'Quiénes somos',
+            'content' => 'Somos una empresa dedicada a la informática con 3 diferentes departamentos...',
+        ];
+
+        return new JsonResponse($aboutUsInfo);
+    }
+
+    /**
+     * @Route("/contact", name="api_contact")
+     */
+    public function getContactInfo(): JsonResponse
+    {
+        $contactInfo = [
+            'email' => 'true_tech@gmail.com',
+            'phone' => '+34 634740280',
+        ];
+
+        return new JsonResponse($contactInfo);
+    }
+}
+?>
